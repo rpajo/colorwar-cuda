2d23988ddfa4d5d6a1864972fc8e585427b46f5b
Rok Pajnič <Rok Pajni>
2016-03-31 13:52:42 +0200

    Dodani komentarji (y)

--- CudaProgram.cu
+++ CudaProgram.cu
@@ -8,20 +8,23 @@
 
 __global__
 void process (int *tabela, int*output, int* random, int vrsta, int pixlov, int offset) {
-	//__shared__ int tabela[3*vrsta*3];
 	int sosedi[3][4];	
 	int stBarv=-1;
+	// x - index pixla -> Blok(vrsta) * število niti v bloku(širina) + zaporedna nit v vrsti
 	int	x =  blockIdx.x*blockDim.x*3 + threadIdx.x*3;
-	//printf("%d\n", x);
+	
+	//sinhronizacija niti - verjentu nepotrebna
 	__syncthreads();
+
+	// Iskanje levega pixla
 	if(x % vrsta*3 != 0){
-		/* Preberemo RGB vrednosti x-1, y pike */
 		stBarv++;
 		sosedi[0][stBarv]= tabela[x-3];
 		sosedi[1][stBarv]= tabela[x-2];
 		sosedi[2][stBarv]= tabela[x-1];
 	}
-				
+		
+	// Iskanje zgornjega pixla		
 	if(x >= vrsta*3){
 		stBarv++;
 		sosedi[0][stBarv]= tabela[x-(vrsta*3)];
@@ -29,6 +32,7 @@ void process (int *tabela, int*output, int* random, int vrsta, int pixlov, int o
 		sosedi[2][stBarv]= tabela[x+2-(vrsta*3)];
 	}
 
+	// Iskanje desnega pixla
 	if( (x == 0) || (x % ((vrsta)*3) != (vrsta-1)*3)){
 		stBarv++;
 		sosedi[0][stBarv]= tabela[x+3];
@@ -36,6 +40,7 @@ void process (int *tabela, int*output, int* random, int vrsta, int pixlov, int o
 		sosedi[2][stBarv]= tabela[x+5];
 	}
 
+	// Iskanje spodnjega pixla
 	if(x < pixlov*3 - vrsta*3){
 		stBarv++;
 		sosedi[0][stBarv]= tabela[x+(vrsta*3)];
@@ -43,14 +48,16 @@ void process (int *tabela, int*output, int* random, int vrsta, int pixlov, int o
 		sosedi[2][stBarv]= tabela[x+2+(vrsta*3)];
 	}
 
-	
+	// če je index na intervalu slike v tabeli - zaradi Cude treba baje prevert da ne uzame krnek
 	if(x < pixlov*3) {
+		// random int iz tabele, vzamemo i-ti element (index pixla) + offset, da ni vedno isti random na pixlu
 		int ran = random[blockIdx.x*blockDim.x + threadIdx.x + offset]%(stBarv+1);
 		output[x] = sosedi[0][ran];
 		output[x+1] = sosedi[1][ran];
 		output[x+2] = sosedi[2][ran];
 		//printf("%d\n", ran);
-		// tabela = output;
+
+		// Izpis za debuggiranje
 		/*printf("pixel: (%d, %d) r:%d sosedi: %d ->  [%d %d %d; %d %d %d; %d %d %d; %d %d %d] -> [%d %d %d]\n", blockIdx.x*blockDim.x, threadIdx.x, ran, stBarv+1,
 			sosedi[0][0], sosedi[1][0], sosedi[2][0], sosedi[0][1], sosedi[1][1], sosedi[2][1], 
 			sosedi[0][2], sosedi[1][2], sosedi[2][2], sosedi[0][3], sosedi[1][3], sosedi[2][3],
@@ -62,6 +69,7 @@ void process (int *tabela, int*output, int* random, int vrsta, int pixlov, int o
 
 int main(int argc, char* argv[]) {
 
+	// Štoparica
 	double diff = 0.0;
 	time_t start;
     time_t stop;
@@ -122,10 +130,12 @@ int main(int argc, char* argv[]) {
 		}
 	}
 
+	// Generiranje random intov v tabelo, ki jo poščjemo na gpu
 	for(j = 0; j < height*width+cudaIteracije; j++) {
 		random[j] = rand();
 	}
 
+	//prenos podatkov iz Hosta na GPU
 	cudaMemcpy(cudaInput, tabela1D, width*height*3*sizeof(int), cudaMemcpyHostToDevice);
 	cudaMemcpy(cudaRandom, random, (cudaIteracije + width*height)*sizeof(int), cudaMemcpyHostToDevice);
 
@@ -134,10 +144,13 @@ int main(int argc, char* argv[]) {
 	char name[64];
 	char datoteka[64];
 	int counter = 1;
+
+	// Klicanje glavne metode v zanki
 	for(i = 0; i < cudaIteracije; i++) {
 		//printf("iteracija: %d\n", i+1);
 		process<<<height, width>>>(cudaInput, cudaOutput, cudaRandom, width, width*height, i);
 
+		// Prenesemo generirano sliko iz GPU na Host in jo nazaj pošjemo kot izvirno sliko
 		cudaMemcpy(rezultat, cudaOutput, width*height*3*sizeof(int), cudaMemcpyDeviceToHost);
 		cudaMemcpy(cudaInput, rezultat, width*height*3*sizeof(int), cudaMemcpyHostToDevice);
 		/*j = 0;
@@ -151,9 +164,9 @@ int main(int argc, char* argv[]) {
 		}
 		printf("------------------------\n");*/
 
-		nova = BMP_Create(width, height, 24);
-
+		// Shranimo vsako 5 sliko v mapo Izhodi
 		if(i != 0 && i%5 == 0 ) {
+			nova = BMP_Create(width, height, 24);
 			for(y = 0; y < height; y++) {
 				for(x = 0; x < width; x++) {
 					BMP_SetPixelRGB(nova, x, y, (unsigned char)rezultat[y*width*3+x*3], 
@@ -170,7 +183,9 @@ int main(int argc, char* argv[]) {
 			BMP_WriteFile( nova, name);
 			}
 	}
-	cudaMemcpy(rezultat, cudaOutput, width*height*3*sizeof(int), cudaMemcpyDeviceToHost);
+
+	//Shranjevanje zadnje slike - nepotrebno, če shranjujemo sproti
+	/*cudaMemcpy(rezultat, cudaOutput, width*height*3*sizeof(int), cudaMemcpyDeviceToHost);
 
 
 	nova = BMP_Create(width, height, 24);
@@ -185,14 +200,16 @@ int main(int argc, char* argv[]) {
 	}
 
 	BMP_WriteFile(nova, argv[2]);
-	BMP_CHECK_ERROR(stdout, -2);
+	BMP_CHECK_ERROR(stdout, -2);*/
 
+	// Sprostimo pomnilnik
 	free(tabela1D);
 	free(random);
 	cudaFree(cudaRandom);
 	cudaFree(cudaInput);
 	cudaFree(cudaOutput);
 
+	// Ustavimo štoparico
 	time(&stop);
   	diff = difftime(stop, start);
   	printf("Runtime: %g\n", diff);
