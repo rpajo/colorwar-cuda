ce5e97461bcfa4d06866edf9d022ad5da5d70215
Rok Pajnič <Rok Pajni>
2016-04-03 20:59:54 +0200

    Pekotova edina naloga

--- obdelavaSlik/prikaz.m
+++ obdelavaSlik/prikaz.m
@@ -1,7 +1,15 @@
 srcFiles = dir('izhodi/*.bmp');
+barve = textread('barve.txt');
 for i = 1 : length(srcFiles)
+    figure(1)
     filename = strcat('izhodi/',srcFiles(i).name);
     I = imread(filename);
     imshow(I);
+    pause(0.2)
+    figure(2)
+    plot(barve(1:i*100, 1), 'r');
+    hold on
+    plot(barve(1:i*100, 2), 'g');
+    plot(barve(1:i*100, 3), 'b');
     %figure, imshow(I);
 end
