b40fe992d78b36116287af7c0f5ad765ad54cefa
gazvoda <gazvoda.lenart@gmail.com>
2016-04-06 21:36:22 +0200

    Pekotov drugi komit

--- obdelavaSlik/prikaz.m
+++ obdelavaSlik/prikaz.m
@@ -5,12 +5,14 @@ for i = 1 : length(srcFiles)
     filename = strcat('izhodi/',srcFiles(i).name);
     I = imread(filename);
     imshow(I);
-    pause(0.2)
+    %pause(0.001)
     figure(2)
     plot(barve(1:i, 1), 'r');
+    title('Vojna barv'), xlabel('Stevilo iteracij'),ylabel('Vsebovanost barv')
+    legend('Rdeca','Zelena','Modra','Oranzna','Location','northwest')
     hold on
     plot(barve(1:i, 2), 'g');
     plot(barve(1:i, 3), 'b');
-    plot(barve(1:i, 4), 'y');
+    plot(barve(1:i, 4),'color',[1.0,0.687,0.387]);
     %figure, imshow(I);
 end
